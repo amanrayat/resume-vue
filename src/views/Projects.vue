c88acdb244766c28f345b4ead6006bd8479d37f0
amanrayat <amanrayat32@gmail.com>
2019-06-08 00:42:57 -0400

    Adding more

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -160,7 +160,7 @@
                                     <v-flex>
                                         <v-card-title primary-title text-color>
                                             <div>
-                                                <div class="headline">Plagiarism Detector</div>
+                                                <div class="headline">Plagiarism Detection</div>
                                                 <span class="grey--text">Source code plagiarism detections application.</span>
                                             </div>
                                         </v-card-title>
